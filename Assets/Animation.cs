0025bcaa7a2bd1fdd38c55b0c64cf099935d3541
ESCG\526737 <526737@chrome.escg.ac.uk>
2021-09-16 14:57:01 +0100

    added punch animation (doesnt punch when in range)

--- Assets/Animation.cs
+++ Assets/Animation.cs
@@ -1,18 +1,41 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Animation : MonoBehaviour
 {
+    public float attackRange;
+    public bool playerInAttackRange;
+    public UnityEngine.AI.NavMeshAgent agent;
+    public Transform player;
+    public LayerMask Player;
+    [SerializeField] private Animator anim;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        playerInAttackRange = Physics.CheckSphere(transform.position, attackRange, Player);
+
+        if (!playerInAttackRange) ChasePlayer();
+        if (playerInAttackRange) AttackPlayer();
+    }
+
+    private void ChasePlayer()
+    {
+        agent.SetDestination(player.position);
+        anim.SetBool("punch", false);
+    }
+
+    private void AttackPlayer()
+    {
+        agent.SetDestination(transform.position);
+        anim.SetBool("punch", true);
     }
 }
